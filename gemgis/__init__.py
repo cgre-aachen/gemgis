6ba263e98b7c2bad4e076c187a416bf3d7728cd4
alexander.juestel <alexander.juestel@rwth-aachen.de>
2020-07-29 22:20:12 +0200

    Set up Example 5

--- gemgis/__init__.py
+++ gemgis/__init__.py
@@ -15,21 +15,29 @@ __correspondence_email__ = 'alexander.juestel@rwth-aachen.de'
 
 __affiliations__ = 'CGRE - RWTH Aachen University'
 
-__version_date__ = '24.07.2020'
+__version_date__ = '30.07.2020'
 
 __version__ = '0.0.x'
 
 __changelog__ = 'What is new in version 0.0.1: \n' \
                 '- Introducing a GemPyData class to store objects like interfaces df, extent, resolution, etc. \n' \
-                '- Extracting XY Coordinates from Point and Line Shape Files \n' \
+                '- Extracting XY Coordinates from vector data (lines, points of shape files, geojsons and gpkg) \n' \
+                '- Interpolate rasters from contour lines \n'\
                 '- Extracting Z values from interpolated rasters and .tif-files \n' \
                 '- Creating GemPy section_dicts from Point and Line Shape Files \n' \
                 '- Calculating slope and aspect of rasters including sampling orientations from rasters \n' \
                 '- Sampling interfaces from rasters \n' \
                 '- Clipping vector and raster data by extents and shapes \n' \
-                '- Rescaling and saving rasters as georeferenced .tif-file\n' \
+                '- Rescaling and saving rasters as georeferenced .tif-files\n' \
                 '- Wrapper functions to plot spatial data in PyVista \n' \
-                '- Parsing of QGIS Style Files (.qml)\n'
+                '- Extracting data from WMS Services \n'\
+                '- Plotting of stereonets for orientation data using mplstereonet \n'\
+                '- Parsing of QGIS Style Files (.qml) to create colors lists for plotting and surface_color_dicts\n'\
+                '- Calculating orientations based on strike lines for layers and faults \n'\
+                '- Export of GemPy geological map as vector data \n'\
+                '- Added extensive testing for all functions and methods \n'\
+                '- Detailed tutorials and examples to demonstrate functionality of GemGIS \n'
+
 __previous_versions__ = '-'
 
 from gemgis.gemgis import *

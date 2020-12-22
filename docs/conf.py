eaa6a21e074e9126d8dcbc89264e501a6ac67752
Alexander Jüstel <alexander.juestel@gmail.com>
2020-12-22 15:52:22 +0100

    'DeactivateNBSphinx_Execute'

--- docs/conf.py
+++ docs/conf.py
@@ -72,4 +72,4 @@ html_theme = 'sphinx_rtd_theme'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
-nbsphinx_execute = 'always'
+nbsphinx_execute = 'never'

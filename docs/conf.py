cbc006ea8d611996033577927428d1b80d6f66c9
Alexander Jüstel <alexander.juestel@gmail.com>
2021-03-17 19:46:23 +0100

    'FixFormatting'

--- docs/conf.py
+++ docs/conf.py
@@ -72,7 +72,7 @@ html_theme = 'sphinx_rtd_theme'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
-nbsphinx_execute = 'always'
+nbsphinx_execute = 'auto'
 
 nbsphinx_execute_arguments = [
     "--InlineBackend.figure_formats={'svg', 'pdf'}",

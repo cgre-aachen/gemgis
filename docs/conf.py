d61981c149f6071414a3492bb5ecbc4eccce6dd8
AlexanderJuestel <45469915+AlexanderJuestel@users.noreply.github.com>
2022-12-29 20:29:29 +0100

    'PrepareRelease'

--- docs/conf.py
+++ docs/conf.py
@@ -24,7 +24,7 @@ copyright = '2022, Alexander Juestel'
 author = 'Alexander Juestel'
 
 # The full version, including alpha/beta/rc tags
-release = '1.0.2'
+release = '1.0.3'
 
 # -- GemGIS configuration ---------------------------------------------------
 sys.path.append('../../gemgis')
